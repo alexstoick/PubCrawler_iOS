eeac8410c6caef027162e4e8fa9a6edcb7782049
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-10-27 01:50:45 +0100

    trying to get the sliders to work with me

---
+++ PubCrawler/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  PubCrawler
+//
+//  Created by Stoica Alexandru on 10/27/13.
+//  Copyright (c) 2013 Stoica Alexandru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property (nonatomic) IBOutlet BasicSlider *moneySlider;
+@property (nonatomic) IBOutlet BasicSlider *pubsSlider;
+
+
+@end
---
+++ PubCrawler/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  PubCrawler
+//
+//  Created by Stoica Alexandru on 10/27/13.
+//  Copyright (c) 2013 Stoica Alexandru. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
